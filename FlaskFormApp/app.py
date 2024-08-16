52583c826907c46be65af5a4ed5d4cb02107154a
louieTW <141008928+louieTW@users.noreply.github.com>
2024-08-16 17:48:46 +0800

    Update app.py
    Signed-off-by: louieTW <141008928+louieTW@users.noreply.github.com>

--- FlaskFormApp/app.py
+++ FlaskFormApp/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Configure Flask-Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Replace with your email provider's SMTP server
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'louie08111louie@gmail.com'  # Replace with your email
